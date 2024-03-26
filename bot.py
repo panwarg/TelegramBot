902cb9a7002106ce93b7c7aff731f91b8fd8d44d
Gaurav <gaurav@equalto.io>
2024-03-26 07:53:54 +0530

    While loop for waiting of deposit.

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import telebot
 from dotenv import load_dotenv
 import requests
 import uuid
+import time
 
 load_dotenv()
 
@@ -24,8 +25,20 @@ def send_welcome(message):
     response += "Send to: " + WALLET_ADDRESS + "\r\n\r\n"
     response += "Comment: " + str(comment) + "\r\n\r\n"
     response += "Important! Don't forget to include comment in transaction!"
-
     bot.reply_to(message, response)
+    retry_counts = 0
+    while retry_counts < 100:
+        retry_counts += 1
+        time.sleep(5)
+        ton_txs = get_ton_transactions()
+        response = "This comment is NOT available in transactions of this wallet: " + WALLET_ADDRESS
+        if str(comment) in ton_txs.text:
+            bot.reply_to(message, "Wallet is connected!")
+            break
+    
+    if retry_counts > 99:
+        bot.reply_to(message, "Waited too long but wallet was not connected.")
+
 
 
 @bot.message_handler(func=lambda msg: True)
